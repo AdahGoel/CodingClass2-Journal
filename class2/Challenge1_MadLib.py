83f88e28c59d21664682c1796c02fe9b8cfd79f1
AdahGoel <adahgoel@outlook.com>
2019-11-16 10:10:13 -0800

    Create Challenge1_MadLib.py

---
+++ class2/Challenge1_MadLib.py
@@ -0,0 +1,8 @@
+# MadLib.py
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+animal = input("Please enter an animal: ")
+place = input("Please enter a place: ")
+print("Your MadLib:")
+print("The",adjective, noun, "jumped on the", animal ,"and rode all the way to" ,place)
+
